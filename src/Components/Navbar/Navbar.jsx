a75bd8779c701b3a11068fc4cbc7aac4e999844c
wtrantan <wtrantan@gmail.com>
2023-09-21 11:47:28 -0700

    new navbar and logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,40 +1,33 @@
 import './Navbar.css'
-import { Routes, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route, Link, NavLink } from "react-router-dom";
 import Main from '../../pages/Main/Main';
 import Contacts from '../../pages/Contacts/Contacts';
 export default function Navbar() {
-    function toggleMobileMenu(menu) {
-        menu.classList.toggle('open');
-    }
+  const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-    <div>
-  <header>
-
-<div className="tw-text-center "><a id='brand' className="" href="/"><img src='tht-logo.png'></img></a></div>
-
-</header>
-
-<nav>
-  <ul>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="Stories">Stories</Link></li>
-    <li><Link to="Contacts">About Us</Link></li>
-    
-  </ul>
-</nav>
-
-<div id="hamburger-icon" onClick={() => toggleMobileMenu(this)}>
-  <div className="bar1"></div>
-  <div className="bar2"></div>
-  <div className="bar3"></div>
-  <ul className="mobile-menu">
-    <li><a href="/home">Home</a></li>
-    <li><a href="/stories">Stories</a></li>
-    <li><a href="/about">About Us</a></li>
-  </ul>
-</div>
-
-</div>
+      <nav>
+      <Link to="/" className="title">
+      <img id='logo' src='tht-logo.png'></img>
+      </Link>
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/Stories">Stories</NavLink>
+        </li>
+        <li>
+          <NavLink to="/Contacts">About Us</NavLink>
+        </li>
+      </ul>
+    </nav>
 
     )
 }
--- src/pages/Stories/Stories.jsx
+++ src/pages/Stories/Stories.jsx
@@ -9,12 +9,12 @@ function Stories() {
     
     <Container fluid className= "">
       <Row>
-      <Col xs={6} md={4}><a><img src="https://i.guim.co.uk/img/media/f77d607c8607ed032cf3100a69ddaf48eb63643d/0_20_1024_614/master/1024.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0f16c69fc14e1a5d2f833d34bb3063b1"></img></a></Col>
-      <Col xs={6} md={4}><img src="https://i.ytimg.com/vi/g_gB_x0jP6o/maxresdefault.jpg"></img></Col>
-      <Col xs={6} md={4}><img src="logo192.png"></img></Col>
-      <Col xs={6} md={4}><img src="logo192.png"></img></Col>
-      <Col xs={6} md={4}><img src="logo192.png"></img></Col>
-      <Col xs={6} md={4}><img src="logo192.png"></img></Col>
+      <Col xs={6} md={4}><a><img  id="grid" src="https://i.guim.co.uk/img/media/f77d607c8607ed032cf3100a69ddaf48eb63643d/0_20_1024_614/master/1024.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0f16c69fc14e1a5d2f833d34bb3063b1"></img></a></Col>
+      <Col xs={6} md={4}><img  id="grid" src="https://i.ytimg.com/vi/g_gB_x0jP6o/maxresdefault.jpg"></img></Col>
+      <Col xs={6} md={4}><img  id="grid" src="https://pbskids.org/apps/media/apps/app_icon_iOS.png"></img></Col>
+      <Col xs={6} md={4}><img  id="grid" src="https://pbskids.org/apps/media/apps/app_icon_iOS.png"></img></Col>
+      <Col xs={6} md={4}><img  id="grid" src="https://pbskids.org/apps/media/apps/app_icon_iOS.png"></img></Col>
+      <Col xs={6} md={4}><img  id="grid" src="https://pbskids.org/apps/media/apps/app_icon_iOS.png"></img></Col>
       </Row>
     </Container>
     </div>
